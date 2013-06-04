8670df40aaab62c72f2c3f0e81a7c30151a22e91
geeklhem <geeklhem@volte.(none)>
2013-06-04 11:46:13 +0200

    Compatibility python 2.6

--- component/test.py
+++ component/test.py
@@ -3,19 +3,19 @@
 """Test components"""
 
 def attach(self):
-    print("Fontion attach in module :  {}".format(__name__))
+    print("Fonction attach in module :  {0}".format(__name__))
 
 
 def bad(self):
-    print("Fontion bad in module :  {}".format(__name__))
+    print("Fonction bad in module :  {0}".format(__name__))
 
 
 def payoff(self,n):
     return(0)
 
 def dispersion(self):
-    print("Fontion dispersion in module :  {}".format(__name__))
+    print("Fonction dispersion in module :  {0}".format(__name__))
 
 def init(self):
-    print("Fontion init in module :  {}".format(__name__))
+    print("Fonction init in module :  {0}".format(__name__))
 
