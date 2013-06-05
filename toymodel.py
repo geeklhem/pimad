5446091d30d48f0fb75c969823b39df8b264657d
geeklhem <geeklhem@volte.(none)>
2013-06-05 14:17:58 +0200

    Aggregation function for the toymodel

--- toymodel.py
+++ toymodel.py
@@ -3,7 +3,10 @@
 """The toy model as defined by Garcia & De Monte 2012."""
 
 import numpy
+import random
+
 import model
+
 
 class ToyModel(model.Model):
     """A toy model of group formation by differential attachment. [Garcia & De Monte 2012]
@@ -11,15 +14,17 @@ class ToyModel(model.Model):
 
     Needs the following parameters in *model.param* dict : 
     
-    =====  ========================================================
+    =====  =====================================================================
     Name   Parameter
-    =====  ========================================================
+    =====  =====================================================================
     b      Benefits in the pgg (int)
     c      Linear cost of altruism (int)
-    ps     Attachment probability of a social individual   (float).
-    pa     Attachment probability of an asocial individual (float).
-    mu     Mutation rate
-    =====  ========================================================
+    ps     Attachment probability of two social individuals  (float).
+    pa     Attachment probability of two asocial individuals (float).
+    pas    Attachment probability of an social and an asocial individual (float).
+    mu     Mutation rate (float).
+    =====  =====================================================================
+
     
     """
 
@@ -45,7 +50,40 @@ class ToyModel(model.Model):
         =========  ============================
   
         """
-        pass
+        
+        ## For each patch
+        for patch in range(self.population.Npatch):
+            # Here patch size is assumed to always be T.
+            patch_begin = patch * self.population.T
+            patch_end = patch_begin + self.population.T
+
+            ## Choose a random recruiter and compute the probabilities to attach.
+            recruiter_index = random.randint(patch_begin,patch_end)
+            
+            if self.population.phenotype[recruiter_index]:
+                # The recruiter is social.
+                proba = (self.param["pas"],self.param["ps"])
+            else: 
+                # The recruiter is asocial.
+                proba = (self.param["pa"],self.param["pas"])
+
+            ## Loop on patch individuals and give them a chance to attach to the group.
+            for n,i in enumerate(self.population.phenotype[patch_begin:patch_end]):
+                if random.random() < proba[i]:
+                    self.population.repartition[n+patch_begin] = 1
+                    self.population.proportions[patch,0] += i #= 1 if social
+                    self.population.proportions[patch,1] += 1
+                else:
+                    self.population.proportions[patch,2] += i #= 1 if social
+                    self.population.proportions[patch,3] += 1
+                       
+            # The recruiter is always in the group. 
+            if not self.population.repartition[recruiter_index]:
+                self.population.repartition[recruiter_index] = True
+                self.population.proportions[patch,0] += i #= 1 if social
+                self.population.proportions[patch,1] += 1
+                self.population.proportions[patch,2] -= i #= 1 if social
+                self.population.proportions[patch,3] -= 1
 
     def payoff(self):
         """
@@ -109,16 +147,18 @@ class ToyModel(model.Model):
         return phenotype
 
 if __name__ == "__main__":
+    import math
 
-    param = {"N":100000,
-             "T":1000,
+    param = {"N":100,
+             "T":10,
              "b":3,
              "c":1,
              "ps":0.3,
              "pa":0.1,
+             "pas":math.sqrt(0.3*0.1),
              "mu":0.001}
 
-    tracked_values = ["population.N"]
+    tracked_values = ["population.N","population.proportions"]
 
     a = ToyModel(param, tracked_values)
 
