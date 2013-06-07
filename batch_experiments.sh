67558f2a5266aab53f343ffa44842bd363906461
geeklhem <geeklhem@volte.(none)>
2013-06-07 20:12:09 +0200

    Batch experiment for apis (tm) :D

---
+++ batch_experiments.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+python experiment.py toydictyo_ip01.data -g 100 -p 0.1
+python experiment.py toydictyo_ip02.data -g 100 -p 0.2
+python experiment.py toydictyo_ip03.data -g 100 -p 0.3
+python experiment.py toydictyo_ip04.data -g 100 -p 0.4
+python experiment.py toydictyo_ip05.data -g 100 -p 0.5
+python experiment.py toydictyo_ip06.data -g 100 -p 0.6
+python experiment.py toydictyo_ip07.data -g 100 -p 0.7
+python experiment.py toydictyo_ip08.data -g 100 -p 0.8
+python experiment.py toydictyo_ip09.data -g 100 -p 0.9
+python experiment.py toydictyo_ip10.data -g 100 -p 1
+ 
