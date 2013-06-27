8ef896647f2755a677e8e85b6b80a1417f9c0acd
geeklhem <geeklhem@volte.(none)>
2013-06-27 10:24:20 +0200

    Voronoi functions

--- computervision/voronoi.py
+++ computervision/voronoi.py
@@ -9,24 +9,32 @@ import itertools
 import collections
 
 center = np.array(([int(random()*100) for x in range(5)],[int(random()*100) for x in range(5)]))
-colors = ["r","b","g","y","k"]
+
 points = np.array(([int(random()*100) for x in range(100)],[int(random()*100) for x in range(100)]))
-colorlist = [""]*100
+
 
 def d(a,b):
     return math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
 
 plt.scatter(center[0,:],center[1,:],color=colors,s=75)
 
-for k,x,y in itertools.izip(itertools.count(),points[0,:],points[1,:]):
-  nearest = 0
-  mdist = 100
-  for i,cx,cy in itertools.izip(itertools.count(),center[0,:],center[1,:]):
-      dist = d((cx,cy),(x,y))
-      if dist < mdist:
-          mdist = dist
-          colorlist[k] = colors[i]
 
+def voronoi_attribution(center,points):
+    attribution = [0]*len(points[0,:])
+    colors = ["r","b","g","y","k"]
+    for k,x,y in itertools.izip(itertools.count(),points[0,:],points[1,:]):
+        nearest = 0
+        mdist = 100
+        for i,cx,cy in itertools.izip(itertools.count(),center[0,:],center[1,:]):
+            dist = d((cx,cy),(x,y))
+            if dist < mdist:
+                mdist = dist
+                attribution[k] = i                
+    return attribution
+
+
+attribution = voronoi_attribution(center,points)
+colorlist = [colors[i] for i in attribution]
 
 plt.scatter(points[0,:],points[1,:],color=colorlist)
 
@@ -36,9 +44,6 @@ plt.ylim(0,100)
 plt.show()
 
 h = collections.Counter(colorlist)
-print h
-print h.keys()
-print h.values()
 plt.clf()
 ax = plt.gca()
 plt.bar(np.arange(len(h.values())),h.values(), color= h.keys())
