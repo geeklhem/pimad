9e1fcf2331e79a08b878c74d3a48970e59e6134d
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-07-23 11:51:45 +0200

    Multiprocess computing of the PIP

--- pimad/figures/pip.py
+++ pimad/figures/pip.py
@@ -1,9 +1,27 @@
 """ pip.py - Pairwise invasibility plot functions """
 
+from __future__ import division
 from pimad.models.toycontinuous import ToyContinuous
 import numpy as np
 import cPickle as pickle
+import multiprocessing as mp
+import itertools
+import os
 
+def mp_pip(repl=4,model=ToyContinuous,param={},precision=0.1):
+    args = itertools.repeat((model,param,precision),repl)
+    pool = mp.Pool()
+    out = pool.map(agent_star,args)
+    pips = [x[0] for x in out]
+    param = out[0][1]
+    param["replica"] = repl
+    output = np.sum(pips,axis=0)/len(pips)
+    return output,param
+
+def agent_star(args):
+    print "PID =", os.getpid(),
+    return agent_based_pip(*args)
+            
 def agent_based_pip(model=ToyContinuous,param={},precision=0.1):
     """
     Comute th pairwise invasibility plot by successives runs
@@ -12,7 +30,7 @@ def agent_based_pip(model=ToyContinuous,param={},precision=0.1):
     Args:
     
     """
-
+    print "go"
     # Set the parameters.
     default = {
         "n":100,
@@ -41,7 +59,7 @@ def agent_based_pip(model=ToyContinuous,param={},precision=0.1):
             
             #--- Display
             i +=1
-            print("{:0.2%}".format(i/imax))
+            #print("{:0.2%}".format(i/imax))
             #--- 
     
             param["m"] = z
@@ -59,15 +77,21 @@ def agent_based_pip(model=ToyContinuous,param={},precision=0.1):
 
 if __name__ == "__main__":
     import sys
+    r = ''
+    if len(sys.argv) == 3:
+        repl = int(sys.argv[2])
+        r = '_{}repl'.format(repl)
+        pip,param = mp_pip(repl=repl,precision=float(sys.argv[1]))
 
-
-    if len(sys.argv) > 1:
+    elif len(sys.argv) == 2:
         pip,param = agent_based_pip(precision=float(sys.argv[1]))
     else:    
         pip,param = agent_based_pip()
-    with open("pip_{}x{}.pkle".format(*pip.shape),"w") as fi:
+
+    fname = "pip_{0[1]}x{0[1]}{1}.pkle".format(pip.shape,r)
+    with open(fname,"w") as fi:
         pickle.dump((pip,param),fi)
-    print("saved pip_{}x{}.pkle".format(*pip.shape))
+    print(fname.format(*pip.shape))
 
     try:
         import pimad.export.draw as draw
